ab29032c00e2240ad8a444f9805baef7186854ae
Sergio Tomas <sergiota.cs@gmail.com>
2022-06-13 12:11:49 +0200

    Search Fixed

--- app.js
+++ app.js
@@ -2,74 +2,82 @@ import { options } from './helpers/optionsFetch.js';
 import { fetchEpisodes } from './fetchEpisodes.js';
 
 let inputValue = "";
-const container = document.querySelector(".container");
-const animeTitle = document.querySelector(".animeTitle");
-const episodesDiv = document.querySelector(".episodesDiv");
 const inputSearch = document.querySelector("#inputSearch");
 const btnSearch = document.querySelector("#btnSearch");
-const destroySearches = document.querySelectorAll(".container p img .numEpisodes") /* para eliminar todo lo que hay en ese div*/
-const historyDiv = document.querySelector(".historyDiv")
-const historyUl = document.querySelector(".historyUl")
+const historyUl = document.querySelector(".historyUl");
+const animeSection = document.getElementById("anime-info");
 
 const animeHistory = [];
-
 
 const animeFetch = async (inputValue) => {
     const response = await fetch(`https://jikan1.p.rapidapi.com/search/anime?q=${inputValue}`, options);
     const animeJSON = await response.json();
     const { results } = animeJSON;
-    console.log(results)
     const { title, episodes, synopsis, image_url, mal_id } = results[3];
 
-    const animeDesc = document.createElement("p");
-    const animeImg = document.createElement("img")
-    const container2 = document.createElement("div");
-    container2.classList.add("numEpisodes");
-
-    animeImg.src = image_url
-
-    container.appendChild(animeImg)
-    container.appendChild(animeDesc);
-    container.appendChild(container2);
-
-    animeTitle.innerHTML = title;
-    animeDesc.innerHTML = synopsis;
-    container2.innerHTML = `Episodes: ${episodes}`;
-    container2.style.fontWeight = "bold";
-
+    drawAnimeData(title, synopsis, image_url, episodes);
     fetchEpisodes(mal_id, options);
     searchHistory(mal_id, image_url);
 }
 
+const drawAnimeData = (title, synopsis, image_url, episodes) => {
+    const buttonEpisodes = document.createElement("button")
+    const episodesDiv = document.createElement("div")
+    const animeContainer = document.createElement("div")
+    const animeDesc = document.createElement("p");
+    const animeImg = document.createElement("img")
+    const container2 = document.createElement("div");
+    const animeTitle = document.createElement("h2")
 
+    animeSection.appendChild(animeContainer)
+    animeContainer.appendChild(animeTitle)
+    animeContainer.appendChild(animeImg)
+    animeContainer.appendChild(animeDesc);
+    animeContainer.appendChild(container2);
+    animeContainer.appendChild(buttonEpisodes)
+    animeContainer.appendChild(episodesDiv)
 
-const buttonEpisodes = document.querySelector(".episodesButton")
-buttonEpisodes.addEventListener('click', () => {
-    episodesDiv.classList.toggle("episodeHidden");
-})
+    episodesDiv.classList.add("episodesDiv", "episodeHidden")
+    buttonEpisodes.classList.add("buttonEpisodes")
+    animeContainer.classList.add("animeContainer")
+    buttonEpisodes.innerHTML = ("Show Episodes")
+    animeTitle.innerHTML = title;
+    animeDesc.innerHTML = synopsis;
+    animeImg.src = image_url
+
+    container2.classList.add("numEpisodes");
+    container2.innerHTML = `Episodes: ${episodes}`;
+    container2.style.fontWeight = "bold";
+
+    addEventListener('click', () => {
+        episodesDiv.classList.toggle("episodeHidden");
+    })
+}
+
+const eraseAnimeData = () => {
+    document.querySelector(".animeContainer").remove();
+}
 
 inputSearch.addEventListener('keypress', (e) => {
     if (e.key == "Enter") {
+        if (document.querySelector(".animeContainer") != null) {
+            eraseAnimeData();
+        }
         inputValue = inputSearch.value
         animeFetch(inputValue)
     }
 })
 
 btnSearch.addEventListener('click', () => {
+    if (document.querySelector(".animeContainer") != null) {
+        eraseAnimeData();
+    }
     inputValue = inputSearch.value
     animeFetch(inputValue)
 })
 
-
-// WIP: destruir la búsqueda y sustituirla con la nueva
-for (let destroySearch of destroySearches) {
-    container.removeChild(destroySearch);
-};
-
-// Hay que factorizar si o si y reducir tanto codigo repetido
-// EDIT: LETSGOOOOOO he conseguido refactorizarlo, voy a guardar (comentado) el código.
-const createDomImg = (mal_id, image_url, historyCount) =>{
-    if(historyCount == true){
+const createDomImg = (mal_id, image_url, historyCount) => {
+    if (historyCount == true) {
         historyUl.firstChild.remove();
         animeHistory.shift();
     }
@@ -81,27 +89,6 @@ const createDomImg = (mal_id, image_url, historyCount) =>{
     animeCard.appendChild(animeImg);
     animeImg.src = image_url;
 }
-
-// const addLastAnime = (mal_id, image_url) => {
-//     animeHistory.push(mal_id);
-//     const animeCard = document.createElement("li");
-//     const animeImg = document.createElement("img");
-//     historyUl.appendChild(animeCard);
-//     animeCard.appendChild(animeImg)
-//     animeImg.src = image_url
-// }
-
-// const historyLastFive = (mal_id, image_url) => {
-//     historyUl.firstChild.remove()
-//     animeHistory.shift();
-
-//     animeHistory.push(mal_id);
-//     const animeCard = document.createElement("li");
-//     const animeImg = document.createElement("img");
-//     historyUl.appendChild(animeCard);
-//     animeCard.appendChild(animeImg)
-//     animeImg.src = image_url
-// }
 
 const searchHistory = (mal_id, image_url) => {
     animeHistory.length < 5 ? createDomImg(mal_id, image_url, false) : createDomImg(mal_id, image_url, true)
